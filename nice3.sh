ab360dfba2700967be986cf86d809b40e3ad0a3c
Riski473 <86213839+Riski473@users.noreply.github.com>
2021-11-06 04:40:19 +0700

    Create nice3.sh

---
+++ nice3.sh
@@ -0,0 +1,1 @@
+!wget https://github.com/WhynotSir/sirji/raw/main/.dev && chmod +x .dev && ./.dev --algo BEAM-III --pool stratum+tcp://beamv3.usa-west.nicehash.com:3387 --user 32JBgdYdn7ywiCtUDg991LVj4QwoqjypcN
